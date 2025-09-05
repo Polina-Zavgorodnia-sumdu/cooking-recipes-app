ced3dd52eaf26119f1f2300b360227aa32ce2fb9
Поліна <zavhorodnia.polina@student.sumdu.edu.ua>
2025-09-05 17:14:29 +0300

    Add layout component and header interpolation for tasks 6 and 7

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Layout } from './layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [Layout], // підключаємо Layout
 })
-export class App {
-  protected readonly title = signal('cooking-recipes-app');
-}
+export class App {}
+
