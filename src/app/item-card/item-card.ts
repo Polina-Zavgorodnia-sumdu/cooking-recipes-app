ebd9572ce943686a8eec15542427ba2ffe8f12ad
Поліна <zavhorodnia.polina@student.sumdu.edu.ua>
2025-11-04 11:18:21 +0200

    Лабораторна 8: кастомні пайпи та директиви

--- src/app/item-card/item-card.ts
+++ src/app/item-card/item-card.ts
@@ -2,22 +2,25 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { Recipe } from '../core/models/recipe.model';
 import {NgClass, NgForOf, NgIf} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import { ShortenDescriptionPipe } from '../shared/pipes/shorten-description-pipe';
+import { HoverHighlightDirective } from '../shared/directives/hover-highlight';
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
   templateUrl: './item-card.html',
+  styleUrls: ['./item-card.css'],
   imports: [
     NgForOf,
     NgIf,
     NgClass,
-    RouterLink
-  ],
-  styleUrls: ['./item-card.css']
+    RouterLink,
+    ShortenDescriptionPipe,
+    HoverHighlightDirective
+  ]
 })
 export class ItemCard {
   @Input() recipe!: Recipe;
-  // Подія вибору рецепта
   @Output() selectRecipe = new EventEmitter<Recipe>();
   hoveredTag: string | null = null;
   // Метод, що викликається при кліку
--- src/app/items-list/items-list.ts
+++ src/app/items-list/items-list.ts
@@ -1,50 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Recipe } from '../core/models/recipe.model';
-import { ItemCard } from '../item-card/item-card';
-import { DataService } from '../core/services/data';
 import { FormsModule } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { ItemCard } from '../item-card/item-card';
+import { Recipe } from '../core/models/recipe.model';
+import { DataService } from '../core/services/data';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-items-list',
   standalone: true,
   templateUrl: './items-list.html',
   styleUrls: ['./items-list.css'],
-  imports: [CommonModule, ItemCard, FormsModule]
+  imports: [CommonModule, FormsModule, ItemCard]
 })
-export class ItemsList implements OnInit, OnDestroy {
-  recipes: Recipe[] = [];
+export class ItemsList {
   searchTerm: string = '';
+  recipes$!: Observable<Recipe[]>;
 
-  private recipesSubscription!: Subscription;
-
-  constructor(private dataService: DataService) {}
-
-  ngOnInit(): void {
-    // 🔹 Підписка на BehaviorSubject (recipes$)
-    this.recipesSubscription = this.dataService.recipes$.subscribe({
-      next: (data: Recipe[]) => {
-        this.recipes = data;
-      },
-      error: (err: any) => console.error('Помилка під час підписки:', err),
-      complete: () => console.log('Підписку завершено.')
-    });
+  constructor(private dataService: DataService) {
+    this.recipes$ = this.dataService.recipes$;
   }
 
-  // 🔹 Викликає фільтрацію в сервісі
   onSearchChange(): void {
     this.dataService.filterRecipes(this.searchTerm);
-  }
-
-  onRecipeSelected(recipe: Recipe): void {
-    console.log('Обраний рецепт:', recipe.title);
-  }
-
-  ngOnDestroy(): void {
-    if (this.recipesSubscription) {
-      this.recipesSubscription.unsubscribe();
-      console.log('Відписка від BehaviorSubject виконана.');
-    }
   }
 }
